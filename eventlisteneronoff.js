f454856d04e6fc128796dd1a9f84e8da4b08738d
Flashfundbeta <119822902+Flashfundbeta@users.noreply.github.com>
2022-12-05 16:01:52 +0100

    Delete eventlisteneronoff.js

--- eventlisteneronoff.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load", () => { hasNetwork(navigator.onLine); window.addEventListener("online", () => { // Set hasNetwork to online when they change to online. hasNetwork(true); }); window.addEventListener("offline", () => { // Set hasNetwork to offline when they change to offline. hasNetwork(false); }); });
